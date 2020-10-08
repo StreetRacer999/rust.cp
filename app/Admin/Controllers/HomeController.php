316fb528d862e756375fda9782c03e87b8f3f3da
unknown <streetracer.2015@yandex.ru>
2020-10-08 21:10:29 +0400

    main

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -13,6 +13,7 @@ class HomeController extends Controller
 {
     public function index(Content $content)
     {
+
         return $content
             ->header('Dashboard')
             ->description('Description...')
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,9 @@ class HomeController extends Controller
      */
     public function index()
     {
+        if(Admin::user()->isRole('administrator')){
+            die('asdasd');
+        }
         return view('home');
     }
 }
